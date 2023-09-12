d109d9c8574badd12440c42af3eb07d880bb90a1
TranTinh2603 <ttinh26032003@gmail.com>
2023-09-12 13:34:45 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/components/InputSearch.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    props: {
+        modelvalue: { type: String, default: "" },
+    },
+    emits: ["submit", "update:modelvalue"],
+    methods: {
+        updateModelvalue(e) {
+            this.$emit("update:modelvalue", e.target.value);
+        },
+        submit() {
+            this.$emit("submit");
+        },
+    },
+};
+</script>
+
+<template>
+    <div class="input-group">
+        <input type="text" class="form-control" placeholder="Nhập thông tin cần tìm" :value="modelValue"
+            @input="updateModelValue" @keyup.enter="submit" />
+        <div class="input-group-append">
+            <button class="btn btn-outline-secondary" type="button" @click="submit">
+                <i class="fas fa-search"></i> Tìm kiếm
+            </button>
+        </div>
+    </div>
+</template>
