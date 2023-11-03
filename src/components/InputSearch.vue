70b7866e368b196a55195bb69d87bf8c51ef445e
TranTinh2603 <ttinh26032003@gmail.com>
2023-11-03 08:15:32 +0700

    Chuc nang tim kiem

--- src/components/InputSearch.vue
+++ src/components/InputSearch.vue
@@ -1,12 +1,12 @@
 <script>
 export default {
     props: {
-        modelvalue: { type: String, default: "" },
+        modelValue: { type: String, default: "" },
     },
-    emits: ["submit", "update:modelvalue"],
+    emits: ["submit", "update:modelValue"],
     methods: {
-        updateModelvalue(e) {
-            this.$emit("update:modelvalue", e.target.value);
+        updateModelValue(e) {
+            this.$emit("update:modelValue", e.target.value);
         },
         submit() {
             this.$emit("submit");
@@ -14,20 +14,13 @@ export default {
     },
 };
 </script>
-
 <template>
     <div class="input-group">
-        <input 
-            type="text" 
-            class="form-control" 
-            placeholder="Nhập thông tin cần tìm" 
-            :value="modelValue"
-            @input="updateModelValue" 
-            @keyup.enter="submit" 
-        />
+        <input type="text" class="form-control" placeholder="Nhập thông tin cần tìm" :value="modelValue"
+            @input="updateModelValue" @keyup.enter="submit" />
         <div class="input-group-append">
             <button class="btn btn-outline-secondary" type="button" @click="submit">
-               <i class="fas fa-search"></i> Tim kiem
+                <i class="fas fa-search"></i> Tìm kiếm
             </button>
         </div>
     </div>
